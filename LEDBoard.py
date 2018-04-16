1873a3b2b3d8195b452cad9888d4cc5356a33042
Deadlygun <sekoiabricks@posteo.de>
2018-04-16 12:49:03 +0000

    neuer code

---
+++ LEDBoard.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+    
+
+from gpiozero import LED
+from time import sleep
+
+red = LED(24)
+
+while True:
+    red.on()
+    sleep(3)
+    red.off()
+    sleep(3)
+    
