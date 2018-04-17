a5ec6ebd2128699614046f47279f21fca84129c9
Gregor Flosdorf <Gregor@Gregors-MacBook.fritz.box>
2018-04-17 15:25:37 +0200

    Kamera

---
+++ schaufenster-roboter/camera_first.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 17 15:21:01 2018
+
+@author: Gregor
+"""
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+
+camera.start_preview()
+sleep(10)
+camera.stop_preview()
