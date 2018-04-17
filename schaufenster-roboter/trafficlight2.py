95292f6c244b4ac71199a0cede183ae054eacfa7
Gregor Flosdorf <Gregor@Gregors-MacBook.local>
2018-04-17 23:23:14 +0200

    Ampel2

---
+++ schaufenster-roboter/trafficlight2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 17 23:22:18 2018
+
+@author: Gregor
+"""
+
+from gpiozero import LED
+from time import sleep
+
+red = LED(2)
+amber = LED(3)
+green = LED(4)
+
+green.on()
+amber.off()
+red.off()
+
+while True:
+    sleep(10)
+    green.off()
+    amber.on()
+    sleep(1)
+    amber.off()
+    red.on()
+    sleep(10)
+    amber.on()
+    sleep(1)
+    green.on()
+    amber.off()
+    red.off()
